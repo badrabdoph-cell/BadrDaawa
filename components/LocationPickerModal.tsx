252a609cade5f0fa0760fd0f524cea907feda746
badrabdoph-netizen <badrabdoph@gmail.com>
2026-06-17 00:38:54 +0300

    Update LocationPickerModal.tsx

--- components/LocationPickerModal.tsx
+++ components/LocationPickerModal.tsx
@@ -211,9 +211,14 @@ export function LocationPickerModal({
       attributionControl: true,
     });
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
       maxZoom: 19,
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
+    }).addTo(map);
+
+    L.tileLayer("https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png", {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>',
     }).addTo(map);
 
     mapRef.current = map;
